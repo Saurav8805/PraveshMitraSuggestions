d279e9c0419ce7927b13e010f060faa1604efbd6
Saurav <sauravambhore@gmail.com>
2026-06-25 16:02:03 +0530

    Update percentile ranges to consecutive ranges: 10-30, 30-40, 40-50...90-100

--- driveConfig.example.js
+++ driveConfig.example.js
@@ -6,41 +6,33 @@ const driveDocuments = {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
     name: '10-30 Percentile College List'
   },
-  '20-40': {
+  '30-40': {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '20-40 Percentile College List'
+    name: '30-40 Percentile College List'
   },
-  '30-50': {
+  '40-50': {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '30-50 Percentile College List'
+    name: '40-50 Percentile College List'
   },
-  '40-60': {
+  '50-60': {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '40-60 Percentile College List'
+    name: '50-60 Percentile College List'
   },
-  '50-70': {
+  '60-70': {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '50-70 Percentile College List'
+    name: '60-70 Percentile College List'
   },
-  '60-80': {
+  '70-80': {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '60-80 Percentile College List'
+    name: '70-80 Percentile College List'
   },
-  '70-90': {
+  '80-90': {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '70-90 Percentile College List'
+    name: '80-90 Percentile College List'
   },
-  '80-100': {
+  '90-100': {
     fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '80-100 Percentile College List'
-  },
-  '90-99': {
-    fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '90-99 Percentile College List'
-  },
-  '99-100': {
-    fileId: 'PASTE_YOUR_FILE_ID_HERE',
-    name: '99-100 Percentile College List (Top Tier)'
+    name: '90-100 Percentile College List (Top Tier)'
   }
 };
 
